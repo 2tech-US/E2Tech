08d2bea15a396a5b8d9bf5645285907d465e8e07
thientoan0101 <thientoancubi@gmail.com>
2022-02-18 21:44:24 +0700

    first commit (setup bottom navigation)
    - date: 18/02/2022
    - by: thientoan
    - content:
        + add bottom navigation
        + setup navigation graph
        + import Glide library (for image loading)

---
+++ app/src/main/java/com/example/e2tech/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.e2tech;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // attach bottom navigation in to home
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        // setup navigation controller
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        // setup default action bar (maybe change with Toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                if (navDestination.getId() == R.id.detailFragment) {
+                    bottomNavigationView.setVisibility(View.GONE);
+                    getSupportActionBar().show();
+                } else {
+                    bottomNavigationView.setVisibility(View.VISIBLE);
+                    getSupportActionBar().hide();
+
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+}
