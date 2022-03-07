a5ba77778206f42b799e3f9cb039a3bdb720764d
thientoan0101 <thientoancubi@gmail.com>
2022-03-07 11:36:41 +0700

    add product list
    - date: 7/3/2022
    - by: thientoan
    - content: add product list when click category

--- app/src/main/java/com/example/e2tech/AdminProductListFragment.java
+++ app/src/main/java/com/example/e2tech/AdminProductListFragment.java
@@ -2,11 +2,29 @@ package com.example.e2tech;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.e2tech.Adapters.AdminProductAdapter;
+import com.example.e2tech.Models.CategoryModel;
+import com.example.e2tech.Models.ProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +33,66 @@ import android.view.ViewGroup;
  */
 public class AdminProductListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ArrayList<ProductModel> productList;
+    RecyclerView recyclerView;
+    AdminProductAdapter adminProductAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseFirestore db;
+
 
     public AdminProductListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminProductListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static AdminProductListFragment newInstance(String param1, String param2) {
         AdminProductListFragment fragment = new AdminProductListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_product_list, container, false);
+        View root = inflater.inflate(R.layout.fragment_admin_product_list, container, false);
+        productList = new ArrayList<>();
+        String category = getArguments().getString("category");
+        db = FirebaseFirestore.getInstance();
+
+        recyclerView = root.findViewById(R.id.admin_product_recyclerview_in_list_screen);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        adminProductAdapter = new AdminProductAdapter(getActivity(), productList);
+        recyclerView.setAdapter(adminProductAdapter);
+        Log.v("CATEGORY", category);
+
+        db.collection("PopularProducts")
+                .whereEqualTo("type", category)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot documentSnapshot: task.getResult()) {
+                                ProductModel product = documentSnapshot.toObject(ProductModel.class);
+                                String id = documentSnapshot.getId();
+                                product.setId(id);
+
+                                productList.add(product);
+                                adminProductAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+        return root;
     }
 }
