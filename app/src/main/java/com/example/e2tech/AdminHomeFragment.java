c71aea2d6afacab96cea9e8b800898eebde6e901
thientoan0101 <thientoancubi@gmail.com>
2022-03-18 13:40:53 +0700

    add temp item for admin home

--- app/src/main/java/com/example/e2tech/AdminHomeFragment.java
+++ app/src/main/java/com/example/e2tech/AdminHomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.e2tech;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.renderer.XAxisRenderer;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AdminHomeFragment#newInstance} factory method to
@@ -15,33 +26,19 @@ import android.view.ViewGroup;
  */
 public class AdminHomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LineChart saleChart;
+    List<Entry> entries = new ArrayList<Entry>();
+    LineData lineData;
 
     public AdminHomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static AdminHomeFragment newInstance(String param1, String param2) {
         AdminHomeFragment fragment = new AdminHomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +47,7 @@ public class AdminHomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +55,23 @@ public class AdminHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_admin_home, container, false);
+
+        saleChart = root.findViewById(R.id.admin_chart_total_sale);
+//        entries.add(new Entry(3,14));
+//        entries.add(new Entry(5,10));
+//        entries.add(new Entry(7,31));
+//        entries.add(new Entry(10,20));
+
+        for (int i = 1; i<=12; i++) {
+        entries.add(new Entry(i,i*3-i));
+        }
+
+        LineDataSet lineDataSet = new LineDataSet(entries,"country");
+        lineData = new LineData(lineDataSet);
+        saleChart.setData(lineData);
+        saleChart.setVisibleXRangeMaximum(10);
+        saleChart.invalidate();
+        return root;
     }
 }
