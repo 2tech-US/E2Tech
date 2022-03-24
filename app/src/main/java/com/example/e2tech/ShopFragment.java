2c1409d4aee018ded5678d189999eeded069d316
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2022-03-24 16:31:14 +0700

    Add: Recycleview With Pagination

--- app/src/main/java/com/example/e2tech/ShopFragment.java
+++ app/src/main/java/com/example/e2tech/ShopFragment.java
@@ -5,10 +5,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
@@ -20,10 +23,16 @@ import com.example.e2tech.Adapters.CategoryAdapter;
 import com.example.e2tech.Adapters.PopularAdapter;
 import com.example.e2tech.Models.CategoryModel;
 import com.example.e2tech.Models.ProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,14 +53,20 @@ public class ShopFragment extends Fragment {
     CategoryAdapter categoryAdapter;
     List<CategoryModel> categoryModelList;
 
-    RecyclerView popularRecyclerView;
-    PopularAdapter popularAdapter;
+    RecyclerView productRecycleView;
+    PopularAdapter productAdapter;
     ArrayList<ProductModel> productList;
 
     TextView tvSeeAll;
     NavController navController;
 
     SearchView searchView;
+
+    private int limit_query = 2;
+    private DocumentSnapshot lastVisible;
+
+    private boolean isScrolling = false;
+    private boolean isLastItemReached = false;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -88,7 +103,7 @@ public class ShopFragment extends Fragment {
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot documentSnapshot: task.getResult()) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                             CategoryModel categoryModel = documentSnapshot.toObject(CategoryModel.class);
                             String id = documentSnapshot.getId();
                             categoryModel.setId(id);
@@ -105,29 +120,143 @@ public class ShopFragment extends Fragment {
         tvSeeAll.setOnClickListener(view -> navController.navigate(R.id.action_shopFragment_to_categoryFragment));
 
         productList = new ArrayList<>();
-        popularRecyclerView = root.findViewById(R.id.shop_product_recycle);
-        popularRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        productRecycleView = root.findViewById(R.id.shop_product_recycle);
+        productRecycleView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 
-        popularAdapter = new PopularAdapter(getActivity(), productList);
-        popularRecyclerView.setAdapter(popularAdapter);
+        productAdapter = new PopularAdapter(getActivity(), productList);
+        productRecycleView.setAdapter(productAdapter);
+
+//        productRecycleView.setNestedScrollingEnabled(false);
 
         assert getArguments() != null;
         String category = getArguments().getString("collection");
-        Log.v("GET",category);
-        db.collection("Products").whereEqualTo("type",category)
+        Log.v("GET", category);
+
+        Query products = db.collection("Products").whereEqualTo("type", category);
+
+//        db.collection("Products").whereEqualTo("type",category).limit(limit_query)
+
+        products.limit(limit_query)
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot documentSnapshot: task.getResult()) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                             ProductModel productModel = documentSnapshot.toObject(ProductModel.class);
                             String id = documentSnapshot.getId();
                             productModel.setId(id);
                             productList.add(productModel);
-                            popularAdapter.notifyDataSetChanged();
                         }
+                        productAdapter.notifyDataSetChanged();
+
+                        if (!task.getResult().isEmpty()) {
+                            lastVisible = task.getResult().getDocuments().get(task.getResult().size() - 1);
+
+                            productRecycleView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                                @Override
+                                public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                                    super.onScrollStateChanged(recyclerView, newState);
+                                    if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                                        isScrolling = true;
+                                    }
+                                    // this is alternative solution
+                                    if (!recyclerView.canScrollVertically(1)) {
+                                        GridLayoutManager gridLayoutManager = (GridLayoutManager) productRecycleView.getLayoutManager();
+//
+                                        int firstVisibleItemPosition = gridLayoutManager.findFirstVisibleItemPosition();
+                                        int visibleItemCount = gridLayoutManager.getChildCount();
+                                        int totalItemCount = gridLayoutManager.getItemCount();
+
+                                        if (isScrolling && ((firstVisibleItemPosition + visibleItemCount) >= totalItemCount) && !isLastItemReached) {
+                                            isScrolling = false;
+
+                                            Log.v("FIST_ITEM", Integer.toString(firstVisibleItemPosition));
+                                            Log.v("VISIBLE_COUNT", Integer.toString(visibleItemCount));
+                                            Log.v("TOTAL_COUNT", Integer.toString(totalItemCount));
+
+                                            Query nextQuery = products.limit(limit_query).startAfter(lastVisible);
+                                            nextQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                                            ProductModel productModel = documentSnapshot.toObject(ProductModel.class);
+                                                            String id = documentSnapshot.getId();
+                                                            productModel.setId(id);
+                                                            productList.add(productModel);
+                                                            Log.v("HEHEHHE", "please work");
+                                                        }
+                                                        productAdapter.notifyDataSetChanged();
+
+                                                        if (!task.getResult().isEmpty()) {
+                                                            lastVisible = task.getResult().getDocuments().get(task.getResult().size() - 1);
+
+                                                            if (task.getResult().size() < limit_query) {
+                                                                isLastItemReached = true;
+                                                                Log.v("ITEM", "LAST");
+                                                            }
+                                                        } else isLastItemReached = true;
+
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }
+
+                                }
+
+                                // onScrolled only call once ? I will use alternative solution
+                                @Override
+                                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                                    super.onScrolled(recyclerView, dx, dy);
+//                                    GridLayoutManager gridLayoutManager = (GridLayoutManager) productRecycleView.getLayoutManager();
+//
+//                                    int firstVisibleItemPosition = gridLayoutManager.findFirstVisibleItemPosition();
+//                                    int visibleItemCount = gridLayoutManager.getChildCount();
+//                                    int totalItemCount = gridLayoutManager.getItemCount();
+//
+//                                    Log.v("FIST_ITEM", Integer.toString(firstVisibleItemPosition));
+//                                    Log.v("VISIBLE_COUNT", Integer.toString(visibleItemCount));
+//                                    Log.v("TOTAL_COUNT", Integer.toString(totalItemCount));
+//
+//
+//                                    if (isScrolling && ((firstVisibleItemPosition + visibleItemCount) >= totalItemCount) && !isLastItemReached) {
+//                                        isScrolling = false;
+//
+//                                        Query nextQuery = products.limit(limit_query).startAfter(lastVisible);
+//                                        nextQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                                            @Override
+//                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                                if (task.isSuccessful()) {
+//                                                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+//                                                        ProductModel productModel = documentSnapshot.toObject(ProductModel.class);
+//                                                        String id = documentSnapshot.getId();
+//                                                        productModel.setId(id);
+//                                                        productList.add(productModel);
+//                                                        Log.v("HEHEHHE","please work");
+//                                                    }
+//                                                    productAdapter.notifyDataSetChanged();
+//
+//                                                    if(!task.getResult().isEmpty()) {
+//                                                        lastVisible = task.getResult().getDocuments().get(task.getResult().size() - 1);
+//
+//                                                        if (task.getResult().size() < limit_query) {
+//                                                            isLastItemReached = true;
+//                                                            Log.v("ITEM", "LAST");
+//                                                        }
+//                                                    }
+//                                                    else isLastItemReached = true;
+//
+//                                                }
+//                                            }
+//                                        });
+//                                    }
+                                }
+                            });
+                        }
+
                     } else {
                         Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
-                        Log.e("FIREBASE","ERROR" + task.getException());
+                        Log.e("FIREBASE", "ERROR" + task.getException());
                     }
                 });
 
@@ -138,7 +267,7 @@ public class ShopFragment extends Fragment {
         categoryModelList = new ArrayList<>();
         categoryRecyclerView = root.findViewById(R.id.product_category_recycle);
         categoryRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
-        categoryAdapter = new CategoryAdapter(getActivity(), categoryModelList,R.layout.category_item_home);
+        categoryAdapter = new CategoryAdapter(getActivity(), categoryModelList, R.layout.category_item_home);
         categoryRecyclerView.setAdapter(categoryAdapter);
     }
 }
