9dd4cf05fb782f1f7d54e4451351b1fbd3b7d54f
minhduc <leminhduc1062001@gmail.com>
2022-03-24 21:04:18 +0700

    add banner (Admin about)

--- app/src/main/java/com/example/e2tech/AdminAboutFragment.java
+++ app/src/main/java/com/example/e2tech/AdminAboutFragment.java
@@ -3,15 +3,28 @@ package com.example.e2tech;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.e2tech.Activities.LoginActivity;
+import com.example.e2tech.Adapters.BannerSliderAdapter;
+import com.example.e2tech.Models.BannerModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,11 +34,11 @@ import com.google.firebase.auth.FirebaseAuth;
 public class AdminAboutFragment extends Fragment {
 
     Button btnLogout;
-    FirebaseAuth auth;
-    public AdminAboutFragment() {
-
-    }
-
+    FirebaseAuth mAuth;
+    SliderView bannerSliderView;
+    List<BannerModel> bannerList;
+    BannerSliderAdapter bannerSliderAdapter;
+    FirebaseFirestore db;
 
     public static AdminAboutFragment newInstance(String param1, String param2) {
         AdminAboutFragment fragment = new AdminAboutFragment();
@@ -47,7 +60,8 @@ public class AdminAboutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_admin_about, container, false);
-        auth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         btnLogout = root.findViewById(R.id.btn_admin_logout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +74,28 @@ public class AdminAboutFragment extends Fragment {
                 getActivity().finishAffinity();
             }
         });
+
+        bannerList = new ArrayList<>();
+        bannerSliderView = root.findViewById(R.id.imageBannerSlider);
+        bannerSliderAdapter = new BannerSliderAdapter(getActivity(), bannerList);
+        bannerSliderView.setSliderAdapter(bannerSliderAdapter);
+
+        db.collection("Banners")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                BannerModel bannerModel = documentSnapshot.toObject(BannerModel.class);
+                                bannerList.add(bannerModel);
+                                bannerSliderAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
         return root;
     }
 }
