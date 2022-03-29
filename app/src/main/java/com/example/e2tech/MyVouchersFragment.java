a3344e5e5e402f980725fa77ee64f06889502fd0
minhduc <leminhduc1062001@gmail.com>
2022-03-29 17:33:13 +0700

    update MyVouchers fragment

--- app/src/main/java/com/example/e2tech/MyVouchersFragment.java
+++ app/src/main/java/com/example/e2tech/MyVouchersFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.e2tech.Adapters.VoucherAdapter;
 import com.example.e2tech.Models.VoucherModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -27,7 +33,31 @@ public class MyVouchersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_vouchers, container, false);
+        voucherList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
 
+        recyclerView = view.findViewById(R.id.myvoucher_list_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        voucherAdapter = new VoucherAdapter(getActivity(), voucherList);
+        recyclerView.setAdapter(voucherAdapter);
+
+        db.collection("Promotions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot documentSnapshot: task.getResult()) {
+                                VoucherModel voucher = documentSnapshot.toObject(VoucherModel.class);
+
+                                voucherList.add(voucher);
+                                voucherAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
         return view;
     }
 }
