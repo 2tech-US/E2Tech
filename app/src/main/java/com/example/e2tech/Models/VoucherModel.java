5e5fc66d7dee575e11cc5cb153f8d601cca56e15
minhduc <leminhduc1062001@gmail.com>
2022-03-29 15:41:18 +0700

    add VoucherAdapter

--- app/src/main/java/com/example/e2tech/Models/VoucherModel.java
+++ app/src/main/java/com/example/e2tech/Models/VoucherModel.java
@@ -10,6 +10,15 @@ public class VoucherModel extends ViewModel implements Serializable {
     String description;
     String img_url;
 
+    public VoucherModel() {}
+
+    public VoucherModel(String code, double discount, String description, String img_url) {
+        this.code = code;
+        this.discount = discount;
+        this.description = description;
+        this.img_url = img_url;
+    }
+
     public String getCode() {
         return code;
     }
